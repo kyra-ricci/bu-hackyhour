64e75ddfe9f3597f0539f1603c8ca8c6847649ab
Kyra Ricci <kyra.d.ricci@gmail.com>
2024-07-09 14:08:00 -0400

    KR 7/9/24

---
+++ statrethink_su24/ch5_practiceprobs.R
@@ -0,0 +1,32 @@
+# Easy -------------------------------------
+
+## 5E1
+# 2, 4
+
+## 5E2
+
+# Animal diversity is linearly related to latitude, but only after controlling for plant diversity
+
+# Where 
+# Lat = mean latitude
+# AD = animal diversity
+# PD = plant diversity
+
+# ADi <- a + (bLat * Lat) + (bPD * PD)
+
+## 5E3 
+
+# Neither amount of funding nor size of laboratory by itself id a good predictor of time to PhD degree; but together these variables are both positively associated with time to degree
+
+# Where
+# Degree = Time to PhD degree
+# Funding = Funding
+# Size = Size of lab
+
+# Degree = a + (bFunding * Funding) + (bSize * Size)
+# bFunding and bSize should both be positive (???)
+
+## 5E4
+
+# 1, 3, 4, 5
+
