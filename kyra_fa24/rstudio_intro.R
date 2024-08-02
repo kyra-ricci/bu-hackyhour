8dc289e38d6c1217d80e5faa2da571300653b7d0
Kyra Ricci <kyra.d.ricci@gmail.com>
2024-08-02 16:20:41 -0500

    Emily additions 8/2/24

--- kyra_fa24/rstudio_intro.R
+++ kyra_fa24/rstudio_intro.R
@@ -84,3 +84,140 @@ plot(y,x)
 ?lm
 ?data.frame
 # Press back and forward arrows in help tab
+
+# Basic Atomic Classes and Data Structures --------------------------------
+
+#5 main types of atomic classes (depending on who you ask)
+#Logical (T/F)
+#integer (2L)
+#numeric (real or decimal)
+#complex (3i)
+#character('a')
+
+##Different Types of Data Structures: ----------------
+
+#vectors: most common data structure in R
+#Two types: atomic vectors (vectors of characters, logical, integers, or numeric) and lists 
+
+#general pattern of a vector:
+#x <- vector(class of object, length)
+#you can also use c() to make a vector -- c() stands for concatenate
+
+#Let's make an empty vector:
+a <- c()
+#this creates an object in your global environment named 'a'. If you take a look in your environment, you'll see that the value stored in 'a' is NULL
+
+#Let's add some values to 'a'
+a <- c(0, 1, 1, 2, 3, 5, 8, 13, 21) #creates a numeric vector
+
+#Let's make a character vector
+flowers <- c("sunflower", "rose", "lily", "daisy", "tulip") #notice how we popped them in quotes and it turned the text green? If we leave the words out of quotation marks, R doesn't recognize it as text and will think you are typing a command incorrectly. Try leaving out the quotes and see what happens.
+
+#Matrices: a special type of vector - contains rows and columns (is a 2D data structure)
+
+#let's make a matrix that stores values 1-9
+#we'll want to save it as an object, so let's give it a name
+max <- matrix(1:9, nrow = 3, ncol = 3) #the matrix() function creates the matrix. 1:9 tells R that we want to include values 1 through 9 (':' denotes that in R) and nrow/ncol= 3 tells R we want three rows and three columns
+
+#let's view max
+max
+#you'll see we have three rows and three columns, but they are unlabeled. Take a closer look at you'll see all of the numbers for columns have a comma in front of them and all of the numbers for rows have a comma after them. This has to do with bracket ([n,m]) notation in R. The notation is [row, column].
+
+#what if we want to give our rows and columns names?
+colnames(max) <- c("A", "B", "C") #here we are telling R we want to store the following column names in max
+rownames(max) <- c("a", "b", "c") #here we are telling R we want to store the following row names in max
+
+#let's view it again
+max
+#now we have row and column names!
+
+#lists: lists are vectors that contain other objects. They can have any data type, so each element of a list can be a different class.
+
+stuff <- list(1, "beetle", TRUE, 1+4i) #here we have a list with classes of number, character, logical, and complex
+str(stuff) #this lets you check the structure of your data.
+
+#Factors: special vectors that represent categorical data. These are really useful when you are modeling and plotting - we'll come back to these later!
+
+#data frames: data structure for most tabular data and what we use for statistics. We generally create them in R using read.csv() or read.table(), but this week we are going to use a dataset already built into R.
+
+#Iris is a dataset built into R. It was introduced by Ronald Fisher in his paper 'The use of multiple measurements in taxonomic problems'. It has data for three plant species (setosa, virginica, and versicolor) and four measurements for each species. Let's take a look at the data
+
+#let's save it as an object in R so we can manipulate it
+dat <- iris
+
+#lets take a look at the first six rows of data
+head(dat) #head() automatically lets you look at the first six rows.
+#What if we want to look at the first 10 rows?
+head(dat, n = 10) #adding n= 10 tells R we want to look at a different number of rows.
+
+#What about bracket notation?
+dat[1:6,] #this tells R we want to look at rows 1-6 and all columns
+
+#What if we want to see the last six rows of data?
+tail(dat) #tail() works just like head() but shows us the 'tail' end of the data instead of the beginning
+
+#Lets check the structure of our data
+str(dat) #all variables are numeric except Species - it is already set to a factor
+#what about the dimensions of our data?
+dim(dat) #this tells us we have a data frame with 150 rows and 5 columns
+#We can check this individually using the following commands:
+nrow(dat) #gives us the number of rows in dat
+ncol(dat) #gives us the number of columns in dat
+
+#what if we just want the column names?
+names(dat)
+
+#What if we are only interested in one column? How can we take a look at it?
+dat$Species #the '$' is a logical operator that tells R we are interested in the "Species" column in the data frame dat
+
+#If we want to make a new column in dat, we can use the "$" operator
+dat$test <- 1 #this tells R that in the data frame 'dat' we want to create a new column named "test" and we want to store '1' in that column.
+#Let's take a look at the structure of our data:
+str(dat)
+#you'll see that we now have a sixth column called 'test' that has '1' stored in it!
+
+#What if we are only interested in one of the three species? Can we use the '$' operator to create a separate data frame with only 'virginica'?
+#Yes! What we will need to do is subset the data to include only values related to 'virginicia'. We can do this by creating a new object in R. We'll call it 'virginica'.
+virginica <- subset(dat, dat$Species == 'virginica')
+#the subset function tells R we want to subset the data, but we need to tell R where to look. To do that, we have to include 'dat' followed by a comma and dat$Species. This tells R we are using the dat data frame and we are specifically interested in the 'Species' column. The '==' is a logical operator and it is telling R we are only interested in values that are 'absolutely equal to' virginica.
+
+#let's check out our new data
+head(virginica)
+dim(virginica) #now we have a data frame of 50 rows and 6 columns
+
+#What if we are interested in Virginica and Setosa but NOT Versicolor?
+vs <- subset(dat, dat$Species != "versicolor") #we are using the subset function again, but we've changed our logical operator. Here, the "!" means "NOT". The "!=" tells R we are interested in all species in dat except for versicolor.
+
+#lets check out our new data frame
+dim(vs) #we now have a data frame of 100 rows and 6 columns
+
+#how can we be sure that we didn't accidentally include 'versicolor'?
+which(vs == 'versicolor') #this command is asking R which of the variables in R is absolutely equal to versicolor
+#integer(0) tells us that none of the values stored in vs are versicolor.
+
+#What if we are interested in the mean value of Sepal Width across all species?
+mean(dat$Sepal.Width)
+#What about the maximum and minimum values?
+max(dat$Sepal.Width)
+min(dat$Sepal.Width)
+
+#Here is a link to a great R cheat sheet: https://images.datacamp.com/image/upload/v1697642178/Marketing/Blog/R_Cheat_Sheet_PNG_1.pdf
+
+#This will cover a lot of what we talked about and go a little deeper into some newer functions.
+
+#Basic Plots Using Our Data Frames ---------------------------------------
+
+#what if we want to see a histogram of Sepal length in data?
+hist(dat$Sepal.Length) #the hist() command tells R we want a histogram
+#what about in vs?
+hist(vs$Sepal.Length)
+
+#Try it on one of the other variables!
+
+#What if we are interested in the relationship between petal length and petal width?
+plot(dat$Petal.Width ~ dat$Petal.Length)
+#The above code creates a scatter plot with petal width as the response (dependent) variable and petal length as the independent variable.
+
+#how does this look different when we use our "vs" dataset?
+plot(vs$Petal.Width ~ vs$Petal.Length)
+
